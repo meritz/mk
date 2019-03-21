0912100d99326c5b5767c9fdfa99d7b65eb21b10
Unknown <modestemeritz@gmail.com>
2019-03-21 08:37:39 +0000

    ajustement de image
    ajout image

--- platforms/android/app/build/intermediates/assets/debug/www/main.js
+++ platforms/android/app/build/intermediates/assets/debug/www/main.js
@@ -949,8 +949,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
 /* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
-/* harmony import */ var _ionic_native_splash_screen_ngx__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @ionic-native/splash-screen/ngx */ "./node_modules/@ionic-native/splash-screen/ngx/index.js");
-/* harmony import */ var _ionic_native_status_bar_ngx__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @ionic-native/status-bar/ngx */ "./node_modules/@ionic-native/status-bar/ngx/index.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _ionic_native_splash_screen_ngx__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @ionic-native/splash-screen/ngx */ "./node_modules/@ionic-native/splash-screen/ngx/index.js");
+/* harmony import */ var _ionic_native_status_bar_ngx__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @ionic-native/status-bar/ngx */ "./node_modules/@ionic-native/status-bar/ngx/index.js");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -960,17 +961,100 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 var __metadata = (undefined && undefined.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+
 
 
 
 
 var AppComponent = /** @class */ (function () {
-    function AppComponent(platform, splashScreen, statusBar) {
+    function AppComponent(router, alertCtrl, platform, splashScreen, statusBar) {
+        var _this = this;
+        this.router = router;
+        this.alertCtrl = alertCtrl;
         this.platform = platform;
         this.splashScreen = splashScreen;
         this.statusBar = statusBar;
         this.initializeApp();
+        this.platform.backButton.subscribe(function () {
+            if (_this.routerOutlet && _this.routerOutlet.canGoBack()) {
+                _this.routerOutlet.pop();
+            }
+            else if (_this.router.url === '/tabs/tab1') {
+                navigator['app'].exitApp();
+            }
+            else {
+                _this.presentAlertConfirm();
+            }
+        });
     }
+    AppComponent.prototype.presentAlertConfirm = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var alert;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.alertCtrl.create({
+                            header: 'Confirmer!',
+                            message: 'Voulez-vous quitter l\'application?',
+                            buttons: [
+                                {
+                                    text: 'Annuler',
+                                    handler: function () {
+                                        console.log('Confirm Cancel');
+                                    }
+                                }, {
+                                    text: 'Ok',
+                                    handler: function () {
+                                        console.log('Confirm Ok');
+                                        navigator['app'].exitApp();
+                                    }
+                                }
+                            ]
+                        })];
+                    case 1:
+                        alert = _a.sent();
+                        return [4 /*yield*/, alert.present()];
+                    case 2:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
     AppComponent.prototype.initializeApp = function () {
         var _this = this;
         this.platform.ready().then(function () {
@@ -978,14 +1062,20 @@ var AppComponent = /** @class */ (function () {
             _this.splashScreen.hide();
         });
     };
+    __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["ViewChild"])(_ionic_angular__WEBPACK_IMPORTED_MODULE_1__["IonRouterOutlet"]),
+        __metadata("design:type", _ionic_angular__WEBPACK_IMPORTED_MODULE_1__["IonRouterOutlet"])
+    ], AppComponent.prototype, "routerOutlet", void 0);
     AppComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
             selector: 'app-root',
             template: __webpack_require__(/*! ./app.component.html */ "./src/app/app.component.html")
         }),
-        __metadata("design:paramtypes", [_ionic_angular__WEBPACK_IMPORTED_MODULE_1__["Platform"],
-            _ionic_native_splash_screen_ngx__WEBPACK_IMPORTED_MODULE_2__["SplashScreen"],
-            _ionic_native_status_bar_ngx__WEBPACK_IMPORTED_MODULE_3__["StatusBar"]])
+        __metadata("design:paramtypes", [_angular_router__WEBPACK_IMPORTED_MODULE_2__["Router"],
+            _ionic_angular__WEBPACK_IMPORTED_MODULE_1__["AlertController"],
+            _ionic_angular__WEBPACK_IMPORTED_MODULE_1__["Platform"],
+            _ionic_native_splash_screen_ngx__WEBPACK_IMPORTED_MODULE_3__["SplashScreen"],
+            _ionic_native_status_bar_ngx__WEBPACK_IMPORTED_MODULE_4__["StatusBar"]])
     ], AppComponent);
     return AppComponent;
 }());
@@ -1229,80 +1319,56 @@ var HairtService = /** @class */ (function () {
             {
                 name: 'MK Style',
                 listing: 20,
-                firstImage: '../../assets/imgs/imag1/IMG-20190210-WA0049.jpg',
+                firstImage: '../../assets/imgs/imag1/1.jpg',
                 description: [
-                    '../../assets/imgs/imag1/IMG-20190210-WA0036.jpg',
-                    '../../assets/imgs/imag1/IMG-20190210-WA0037.jpg',
-                    '../../assets/imgs/imag1/IMG-20190210-WA0046.jpg',
-                    '../../assets/imgs/imag1/IMG-20190210-WA0049.jpg',
-                    '../../assets/imgs/imag1/IMG-20190210-WA0040.jpg',
-                    '../../assets/imgs/imag1/IMG-20190210-WA0041.jpg',
-                    '../../assets/imgs/imag1/IMG-20190210-WA0042.jpg',
-                    '../../assets/imgs/imag1/IMG-20190210-WA0045.jpg',
-                    '../../assets/imgs/imag1/IMG-20190210-WA0046.jpg',
-                    '../../assets/imgs/imag1/IMG-20190210-WA0048.jpg',
-                    '../../assets/imgs/imag1/IMG-20190210-WA0049.jpg',
-                    '../../assets/imgs/imag1/IMG-20190210-WA0050.jpg',
+                    '../../assets/imgs/imag1/1.jpg',
+                    '../../assets/imgs/imag1/2.jpg',
+                    '../../assets/imgs/imag1/3.jpg',
+                    '../../assets/imgs/imag1/4.jpg',
                 ],
             },
             {
                 name: 'MK style',
                 listing: 210,
-                firstImage: '../../assets/imgs/img2/IMG-20190210-WA0058.jpg',
+                firstImage: '../../assets/imgs/img2/1.jpg',
                 description: [
-                    '../../assets/imgs/img2/IMG-20190210-WA0051.jpg',
-                    '../../assets/imgs/img2/IMG-20190210-WA0052.jpg',
-                    '../../assets/imgs/img2/IMG-20190210-WA0053.jpg',
-                    '../../assets/imgs/img2/IMG-20190210-WA0054.jpg',
-                    '../../assets/imgs/img2/IMG-20190210-WA0055.jpg',
-                    '../../assets/imgs/img2/IMG-20190210-WA0056.jpg',
-                    '../../assets/imgs/img2/IMG-20190210-WA0057.jpg',
-                    '../../assets/imgs/img2/IMG-20190210-WA0059.jpg',
+                    '../../assets/imgs/img2/1.jpg',
+                    '../../assets/imgs/img2/2.jpg',
+                    '../../assets/imgs/img2/3.jpg',
+                    '../../assets/imgs/img2/4.jpg',
                 ],
             },
             {
                 name: 'zozo',
                 listing: 220,
-                firstImage: '../../assets/imgs/2.jpg',
+                firstImage: '../../assets/imgs/img3/1.jpg',
                 description: [
-                    '../../assets/imgs/11.jpg',
-                    '../../assets/imgs/10.jpg',
-                    '../../assets/imgs/9.jpg',
-                    '../../assets/imgs/8.jpg',
-                    '../../assets/imgs/2.jpg',
-                    '../../assets/imgs/11.jpg',
-                    '../../assets/imgs/10.jpg',
-                    '../../assets/imgs/9.jpg',
+                    '../../assets/imgs/img2/1.jpg',
+                    '../../assets/imgs/img3/2.jpg',
+                    '../../assets/imgs/img3/3.jpg',
+                    '../../assets/imgs/img3/4.jpg',
                 ],
             },
             {
                 name: 'kofi',
                 listing: 270,
-                firstImage: '../../assets/imgs/2.jpg',
+                firstImage: '../../assets/imgs/img4/1.jpg',
                 description: [
-                    '../../assets/imgs/11.jpg',
-                    '../../assets/imgs/10.jpg',
-                    '../../assets/imgs/9.jpg',
-                    '../../assets/imgs/8.jpg',
-                    '../../assets/imgs/2.jpg',
-                    '../../assets/imgs/11.jpg',
-                    '../../assets/imgs/10.jpg',
-                    '../../assets/imgs/9.jpg',
+                    '../../assets/imgs/img4/1.jpg',
+                    '../../assets/imgs/img4/2.jpg',
+                    '../../assets/imgs/img4/3.jpg',
+                    '../../assets/imgs/img4/4.jpg',
                 ],
             },
             {
                 name: 'herve',
                 listing: 280,
-                firstImage: '../../assets/imgs/2.jpg',
+                firstImage: '../../assets/imgs/img5/1.jpg',
                 description: [
-                    '../../assets/imgs/11.jpg',
-                    '../../assets/imgs/10.jpg',
-                    '../../assets/imgs/9.jpg',
-                    '../../assets/imgs/8.jpg',
-                    '../../assets/imgs/2.jpg',
-                    '../../assets/imgs/11.jpg',
-                    '../../assets/imgs/10.jpg',
-                    '../../assets/imgs/9.jpg',
+                    '../../assets/imgs/img5/1.jpg',
+                    '../../assets/imgs/img5/2.jpg',
+                    '../../assets/imgs/img5/3.jpg',
+                    '../../assets/imgs/img5/4.jpg',
                 ],
             },
         ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { IonRouterOutlet, Platform, AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -10,13 +11,52 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+  @ViewChild(IonRouterOutlet) routerOutlet: IonRouterOutlet;
+
   constructor(
+     private router: Router,
+    private alertCtrl: AlertController,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
+    this.platform.backButton.subscribe(() => {
+        if (this.routerOutlet && this.routerOutlet.canGoBack()) {
+          this.routerOutlet.pop();
+        } else if (this.router.url === '/tabs/tab1') {
+          navigator['app'].exitApp();
+        } else {
+          this.presentAlertConfirm();
+        }
+    });
   }
+
+
+   async presentAlertConfirm() {
+    const alert = await this.alertCtrl.create({
+      header: 'Confirmer!',
+      message: 'Voulez-vous quitter l\'application?',
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+            navigator['app'].exitApp();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+    }
+
 
   initializeApp() {
     this.platform.ready().then(() => {
@@ -24,4 +64,5 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
 }
